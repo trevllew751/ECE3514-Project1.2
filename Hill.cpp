c958b9d83ed5a34524864c296e52d9e6e6be2d7d
Trevor <trevorllew@simpleworks.tech>
2021-09-10 05:10:08 -0400

    Implemented inv_mod and other helper methods

--- Hill.cpp
+++ Hill.cpp
@@ -2,30 +2,60 @@
 #include "Hill.hpp"
 
 Hill::Hill() {
-
+    E = Matrix(std::vector<int>{2, 4, 3, 5}, 2, 2);
+    D = inv_mod(E);
 }
 
 Hill::Hill(const Matrix &K, bool encryption) {
-
+    Matrix zero(std::vector<int>(), 0, 0);
+    if (!inv_mod(K).equal(zero)) {
+        if (encryption) {
+            this->E = K;
+            this->D = inv_mod(K);
+        } else {
+            this->D = K;
+            this->E = inv_mod(K);
+        }
+    } else {
+        this->E = zero;
+        this->D = zero;
+    }
 }
 
 Hill::Hill(const Matrix &E, const Matrix &D) {
-
+    Matrix zero(std::vector<int>(), 0, 0);
+    if (!inv_mod(E).equal(D)) {
+        this->E = zero;
+        this->D = zero;
+    } else {
+        this->E = E;
+        this->D = D;
+    }
 }
 
 Matrix Hill::getE() const {
-    return Matrix();
+    return E;
 }
 
 Matrix Hill::getD() const {
-    return Matrix();
+    return D;
 }
 
 bool Hill::setE(const Matrix &E) {
+    if (inv_mod(E).equal(Matrix(std::vector<int>(), 0, 0))) {
+        this->E = E;
+        this->D = inv_mod(E);
+        return true;
+    }
     return false;
 }
 
 bool Hill::setD(const Matrix &D) {
+    if (inv_mod(E).equal(Matrix(std::vector<int>(), 0, 0))) {
+        this->D = D;
+        this->E = inv_mod(D);
+        return true;
+    }
     return false;
 }
 
@@ -58,19 +88,58 @@ std::string Hill::n2let(const Matrix &A) {
 }
 
 Matrix Hill::inv_mod(Matrix A) {
-    return Matrix();
+    if (A.size(1) != A.size(2)) {
+        return Matrix(std::vector<int>(), 0, 0);
+    }
+    // result matrix initialized to identity
+    Matrix result(std::vector<int>(A.size(1) * A.size(2)), A.size(1), A.size(2));
+    for (int i = 0; i < result.size(1); i++) {
+        result.set(i, i, 1);
+    }
+    Matrix mat = A;
+//    int pivot = 0;
+    for (int i = 0; i < A.size(1); i++) {
+        int inverse = ZI29[mat.get(i, i) - 1];
+        row_mult(result, i, 0, result.size(2), inverse);
+        row_mult(mat, i, 0, mat.size(2), inverse);
+        for (int j = 0; j < A.size(1); j++) {
+            if (j != i) {
+                row_diff(result, j, 0, result.size(2), result, i, mat.get(j, i));
+                row_diff(mat, j, 0, mat.size(2), mat, i, mat.get(j, i));
+            }
+        }
+    }
+    return result;
 }
 
+//row_diff(result, j, 0, result.size(2), result, i, result.get(j, k));
+//row_diff(mat, j, 0, mat.size(2), mat, i, mat.get(j, k));
+
 unsigned int Hill::mod(int a, int b) {
-    return 0;
+    if (a < 0) {
+        int n = (std::abs(a) / b) + 1;
+        return a - (-1 * n * b);
+    }
+    return a % b;
 }
 
 void Hill::row_mult(Matrix &A, unsigned int i, unsigned int j, unsigned int k, unsigned int c) {
-
+    //For row i of Matrix A, multiply columns j through k by c, mod 29
+    for (unsigned int col = j; col < k; col++) {
+        A.set(i, col, mod(A.get(i, col) * c, 29));
+    }
 }
 
 void
 Hill::row_diff(Matrix &A, unsigned int i, unsigned int j, unsigned int k, Matrix &B, unsigned int l, unsigned int c) {
-
+    //Multiply columns j through k of row l of Matrix B by c and subtract from columns j through k of row i of Matrix A, mod 29
+//    row_mult(B, l, j, k, c);
+    Matrix m = B;
+    for (unsigned int col = j; col < k; col++) {
+        m.set(l, col, m.get(l, col) * c);
+    }
+    for (unsigned int col = j; col < k; col++) {
+        A.set(i, col, mod(A.get(i, col) - m.get(l, col), 29));
+    }
 }
 
--- student_tests.cpp
+++ student_tests.cpp
@@ -2,13 +2,14 @@
 #include "Hill.hpp"
 #include "Matrix.hpp"
 
-TEST_CASE( "default constructor", "[Hill]" )
+TEST_CASE("default constructor", "[Hill]")
 {
-  INFO("Hint: default constructor (linear getE/D() must work)");
-  Hill LS;
-  Matrix E(std::vector<int>{2,4,3,5}, 2, 2);
-  Matrix D(std::vector<int>{12,2,16,28}, 2, 2);
+    INFO("Hint: default constructor (linear getE/D() must work)");
+    Hill LS;
+    Matrix E(std::vector<int>{2, 4, 3, 5}, 2, 2);
+    Matrix D(std::vector<int>{12, 2, 16, 28}, 2, 2);
+    REQUIRE(LS.getE().equal(E));
+    REQUIRE(LS.getD().equal(D));
 
-  REQUIRE(LS.getE().equal(E));
-  REQUIRE(LS.getD().equal(D));
+
 }
